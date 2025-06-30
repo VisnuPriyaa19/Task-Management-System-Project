de6c72f389ada2866e0502ccdc93c9e912d8264c
srinithi-05 <srinithijk77@gmail.com>
2025-06-30 16:17:37 +0530

    fix: sort for chartjs

---
+++ Backend/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db'); 
+const authRoutes = require('./routes/authRoutes');
+const passwordResetRoutes = require('./routes/passwordResetRoutes')
+const projectRoutes = require('./routes/projectRoutes');
+const taskRoutes = require('./routes/taskRoutes');
+const healthRoute = require('./routes/healthRoutes');
+
+const setupSwagger = require('./config/swagger');
+
+// Load environment variables
+dotenv.config();
+ 
+// Initialize express app - starting point of backend server
+const app = express();
+
+// CORS Configuration
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:4200', 
+  credentials: true         //allows aut headers to be sent across domains
+}));
+
+setupSwagger(app);
+
+// Built-in Middleware to parse JSON request bodies(wihtout this req.body is undefined)
+app.use(express.json());
+ 
+// MongoDB Connection
+connectDB();
+ 
+// Route handlers
+app.use('/api/auth', authRoutes);
+app.use('/api/password', passwordResetRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes); 
+app.use('/api', healthRoute);
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(` Server running on port ${PORT}`);
+  console.log(`Swagger docs at ${PORT}/api-docs`);
+});
